1b5ec4eb5b60d4dd0ec421625c25e2edfb8075eb
Putheng <puthengemail@gmail.com>
2019-03-27 08:34:49 +0000

    update

--- app/Imports/ClinicImport.php
+++ app/Imports/ClinicImport.php
@@ -4,8 +4,9 @@ namespace App\Imports;
 
 use App\Models\Clinic;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class ClinicImport implements ToModel
+class ClinicImport implements ToModel, WithHeadingRow
 {
     /**
     * @param array $row
@@ -14,40 +15,39 @@ class ClinicImport implements ToModel
     */
     public function model(array $row)
     {
-        // dd( $row[30]);
+        // dd($row['code']);
         return new Clinic([
-            'id'            => $this->ifNull($row[0]),
-            'code'          => $this->ifNull($row[1]),
-            'name'          => $this->ifNull($row[2]),
-            'name_lang2'    => $this->ifNull($row[3]),
-            'site_id'       => $this->ifNull($row[4]),
-            'chain_id'      => $this->ifNull($row[5]),
-            'location_desc'         => $this->ifNull($row[6]),
-            'location_desc_lang2'   => $this->ifNull($row[7]),
-            'address_desc'          => $this->ifNull($row[8]),
-            'address_desc_lang2'    => $this->ifNull($row[9]),
-            'directions_desc'       => $this->ifNull($row[10]),
-            'directions_desc_lang2' => $this->ifNull($row[11]),
-            'gps_lat'      => $this->ifNull($row[12]),
-            'gps_long'      => $this->ifNull($row[13]),
-            'email'         => $this->ifNull($row[14]),
-            'phone_num'     => $this->ifNull($row[15]),
-            'website_url'   => $this->ifNull($row[16]),
-            'video_url'     => $this->ifNull($row[17]),
-            'logo_url'      => $this->ifNull($row[18]),
-            'forward_url'   => $this->ifNull($row[19]),
-            'res_notes'     => $this->ifNull($row[20]),
-            'res_notes_lang2'       => $this->ifNull($row[21]),
-            'res_time_slot_length'  => $this->ifNull($row[22]),
-            'sort_index'        => $this->ifNull($row[23]),
+            'code'          => $this->ifNull($row['code']),
+            'name'          => $this->ifNull($row['name']),
+            'name_lang2'    => $this->ifNull($row['name_lang2']),
+            'site_id'       => $this->ifNull($row['site_id']),
+            'chain_id'      => $this->ifNull($row['chain_id']),
+            'location_desc'         => $this->ifNull($row['location_desc']),
+            'location_desc_lang2'   => $this->ifNull($row['location_desc_lang2']),
+            'address_desc'          => $this->ifNull($row['address_desc']),
+            'address_desc_lang2'    => $this->ifNull($row['address_desc_lang2']),
+            'directions_desc'       => $this->ifNull($row['address_desc_lang2']),
+            'directions_desc_lang2' => $this->ifNull($row['directions_desc_lang2']),
+            'gps_lat'      => $this->ifNull($row['gps_lat']),
+            'gps_long'      => $this->ifNull($row['gps_long']),
+            'email'         => $this->ifNull($row['email']),
+            'phone_num'     => $this->ifNull($row['phone_num']),
+            'website_url'   => $this->ifNull($row['website_url']),
+            'video_url'     => $this->ifNull($row['video_url']),
+            'logo_url'      => $this->ifNull($row['logo_url']),
+            'forward_url'   => $this->ifNull($row['forward_url']),
+            'res_notes'     => $this->ifNull($row['res_notes']),
+            'res_notes_lang2'       => $this->ifNull($row['res_notes_lang2']),
+            'res_time_slot_length'  => $this->ifNull($row['res_time_slot_length']),
+            'sort_index'        => $this->ifNull($row['sort_index']),
             'is_enabled'        => 0,
-            'type'              => $this->ifNull($row[25]),
-            'res_options'       => $this->ifNull($row[26]),
-            'res_options_text'  => $this->ifNull($row[27]),
-            'res_options_text_lang2'    => $this->ifNull($row[28]),
-            'days_visible'  => $this->ifNull($row[29]),
-            'created_at'    => $this->ifNull($row[30]),
-            'updated_at'    => $this->ifNull($row[31]),
+            'type'              => $this->ifNull($row['type']),
+            'res_options'       => $this->ifNull($row['res_options']),
+            'res_options_text'  => $this->ifNull($row['res_options_text']),
+            'res_options_text_lang2'    => $this->ifNull($row['res_options_text_lang2']),
+            'days_visible'  => $this->ifNull($row['days_visible']),
+            'created_at'    => $this->ifNull($row['created_at']),
+            'updated_at'    => $this->ifNull($row['updated_at']),
         ]);
     }
 
