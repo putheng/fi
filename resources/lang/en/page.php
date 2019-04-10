e84cb178cc563f5ee79c47c94937a2f234712dbb
Putheng <puthengemail@gmail.com>
2019-04-10 07:54:05 +0000

    multiple language on copy right footer

--- resources/lang/en/page.php
+++ resources/lang/en/page.php
@@ -19,4 +19,7 @@ return [
     'recommend_lang' => 'title_en',
     'recommend_description' => 'description_en',
 
+    'copy_right' => 'This website is made possible by the generous support of the American people through the United States Agency for International Development (USAID) and the U.S. President\'s Emergency Plan for AIDS Relief (PEPFAR). The contents are the responsibility of National Center for HIV/AIDS, Dermatology and STI (NCHADS) and FHI360/LINKAGES Project and do not necessarily reflect the views of USAID, PEPFAR, or the United States Government.',
+    'copy_heading' => 'Copyright © 2018 by'
+
 ];
